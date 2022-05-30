1199236e28565dad96bc5cec96b21170f7066f13
Minsu <jminsu084@gmail.com>
2022-05-31 04:07:05 +0900

    N : SnGBurgerStore에 사용되는 원재료 생성

---
+++ test/src/main/java/test/HashBrown.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author Minsu <jminsu084@gmail.com>
+ */
+public class HashBrown implements Potato{
+    public HashBrown() {
+        System.out.println("Hash Brown");
+    }
+}
