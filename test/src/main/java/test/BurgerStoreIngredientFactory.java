c564ff9098da92a0bc9b9ade414109b0b8f0bff2
Minsu <jminsu084@gmail.com>
2022-05-31 04:09:11 +0900

    N : 팩토리 패턴에 사용되는 BurgerStoreIngredientFactory 생성

---
+++ test/src/main/java/test/BurgerStoreIngredientFactory.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author Minsu <jminsu084@gmail.com>
+ * 
+ * 다 저장해두고 필요하는 것만 가져다 사용할 것임
+ */
+public interface BurgerStoreIngredientFactory {
+    Bun createBun();
+    Cheese createCheese();
+    Potato createPotato();
+    Vegetable createVegetable();
+    Cola createCola();    
+}
