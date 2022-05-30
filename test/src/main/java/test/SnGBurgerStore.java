0dc4761f587889d10ef55766b7d29f96e4a7e346
Minsu <jminsu084@gmail.com>
2022-05-31 04:10:34 +0900

    N : 팩토리 패턴 적용을 위한 버거 가게별로 나누기

---
+++ test/src/main/java/test/SnGBurgerStore.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author Minsu <jminsu084@gmail.com>
+ */
+public abstract class SnGBurgerStore extends BurgerStore {
+    @Override
+    public abstract int cost();
+    
+    @Override
+    public Menu createBurger(String item) {
+        Menu burger = null;
+        BurgerStoreIngredientFactory factory = new SnGBurgerIngredientFactory();
+        
+        if(item.equals("치즈 버거")) {
+            burger = new CheeseBurger(factory);
+            burger.setDescription("SnG Style Cheese Burger");
+        } else if(item.equals("비프 버거")) {
+            burger = new BeefBurger(factory);
+            burger.setDescription("SnG Style Beef Burger");
+        } else if(item.equals("치킨 버거")) {
+            burger = new ChickenBurger(factory);
+            burger.setDescription("SnG Style Chiken Burger");
+        } else if(item.equals("매운 치킨 버거")) {
+            burger = new SpicyChickenBurger(factory);
+            burger.setDescription("SnG Style Spicy Chiken Burger");
+        }
+        
+        return burger;
+    }
+}
