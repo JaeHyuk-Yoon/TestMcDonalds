6dc49f7cf6c41b0c884d6c66d13af29b1d4b4964
Minsu <jminsu084@gmail.com>
2022-05-31 16:11:31 +0900

    N : IndBeefPatty 추가

---
+++ test/src/main/java/test/MediumRare.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author Minsu <jminsu084@gmail.com>
+ */
+public class MediumRare implements IndBeefPatty{
+    @Override
+    public String getIndBeefPatty() {
+        return "MediumRare Beef Patty";
+    }
+}
