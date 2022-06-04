27c7276f23a829f04190576d29f4db13b74488e7
heejin <ghn07096@naver.com>
2022-06-05 02:53:27 +0900

    N : DB관련 클래스 생성

---
+++ test/src/main/java/db/User.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db;
+
+/**
+ *
+ * @author heejin
+ */
+public class User {
+    private String id;
+    private String pw;
+    private String branch;
+    private String type;
+    
+    public User(){
+
+    }
+    
+    public User(String id, String pw, String branch, String type) {
+        this.id = id;
+        this.pw = pw;
+        this.branch = branch;
+        this.type = type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPw() {
+        return pw;
+    }
+
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
